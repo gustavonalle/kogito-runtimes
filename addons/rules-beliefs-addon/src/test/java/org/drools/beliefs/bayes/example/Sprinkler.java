9af87a8e13b72623db438696c3adb294911bfd51
Edoardo Vacchi <uncommonnonsense@gmail.com>
2019-09-04 15:29:22 +0200

    wip: sprinkler pojo

---
+++ addons/rules-beliefs-addon/src/test/java/org/drools/beliefs/bayes/example/Sprinkler.java
@@ -0,0 +1,47 @@
+package org.drools.beliefs.bayes.example;
+
+import org.drools.beliefs.bayes.VarName;
+
+public class Sprinkler {
+
+    @VarName("WetGrass")
+    private double[] wetGrassEvidence;
+    @VarName("Cloudy")
+    private double[] cloudyEvidence;
+    @VarName("Sprinkler")
+    private double[] sprinklerEvidence;
+    @VarName("Rain")
+    private double[] rainEvidence;
+
+    public double[] getWetGrassEvidence() {
+        return wetGrassEvidence;
+    }
+
+    public void setWetGrassEvidence(double[] wetGrassEvidence) {
+        this.wetGrassEvidence = wetGrassEvidence;
+    }
+
+    public double[] getCloudyEvidence() {
+        return cloudyEvidence;
+    }
+
+    public void setCloudyEvidence(double[] cloudyEvidence) {
+        this.cloudyEvidence = cloudyEvidence;
+    }
+
+    public double[] getSprinklerEvidence() {
+        return sprinklerEvidence;
+    }
+
+    public void setSprinklerEvidence(double[] sprinklerEvidence) {
+        this.sprinklerEvidence = sprinklerEvidence;
+    }
+
+    public double[] getRainEvidence() {
+        return rainEvidence;
+    }
+
+    public void setRainEvidence(double[] rainEvidence) {
+        this.rainEvidence = rainEvidence;
+    }
+}
